de71abec9c5b13fe88881e978733b11b0956666e
BuildTools <pcb0011@mix.wvu.edu>
2020-09-26 13:19:59 -0400

    Lab 3, finised login system and made profile page

---
+++ about.php
@@ -0,0 +1,62 @@
+<?php
+require 'includes/header.php'
+?>
+<main>
+<link rel="stylesheet" href="css/about.css"></link>
+
+	<div class= "about">
+			<h1>About Us</h1>
+			<p>Welcome to our movie review site, below are our founding fathers. Each founding father specializes in reviewing a different genres, although they will also review movies outside the genre on occasion.</p>
+	</div>
+	
+	<div class="row">
+		<div class ="column">
+			<div class="card">
+				<img src="images/joe.png" alt="Joe Swanson">
+				<div class="container">
+					<h2>Animated Films</h2>
+					<p>Joe Swanson</p>
+				</div>
+			</div>
+		</div>
+		<div class ="column">
+			<div class="card">
+				<img src="images/brock.png" alt="Brock Sampson">
+				<div class="container">
+					<h2>Action Films</h2>
+					<p>Brock Sampson</p>
+				</div>
+			</div>
+		</div>
+			<div class ="column">
+			<div class="card">
+				<img src="images/kronk.jpg" alt="Kronk">
+				<div class="container">
+					<h2>Documentaries</h2>
+					<p>Kronk</p>
+				</div>
+			</div>
+		</div>
+			<div class ="column">
+			<div class="card">
+				<img src="images/buzz.jpg" alt="Buzz Lightyear">
+				<div class="container">
+					<h2>Crime Films</h2>
+					<p>Buzz Lightyear</p>
+				</div>
+			</div>
+		</div>
+	
+	</div>
+			<button style="background-color: #8741B8;" type="button" class = "center">
+			Click here to see our reviews!
+		</button>
+		
+		
+			
+</main>
+
+
+
+		
+		
